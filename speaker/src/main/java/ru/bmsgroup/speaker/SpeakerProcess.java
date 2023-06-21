6ea0d353aea4eaefb20735611c4307842f28d5d0
den <kdv@groupbms.ru>
2023-06-21 11:00:29 +0300

    add logs

--- speaker/src/main/java/ru/bmsgroup/speaker/SpeakerProcess.java
+++ speaker/src/main/java/ru/bmsgroup/speaker/SpeakerProcess.java
@@ -1,16 +1,18 @@
 package ru.bmsgroup.speaker;
 
+import lombok.extern.java.Log;
 import org.apache.kafka.streams.processor.api.Processor;
 import org.apache.kafka.streams.processor.api.ProcessorSupplier;
 import org.apache.kafka.streams.processor.api.Record;
-
+@Log
 public class SpeakerProcess implements ProcessorSupplier<String, String, String, String> {
     @Override
     public Processor<String, String, String, String> get() {
        return new Processor<String,String,String,String>() {
             @Override
             public void process(Record<String, String> record) {
-
+                log.info("record key: "+ record.key());
+                log.info("record value: "+ record.value());
             }
         };
     }
