4cad9fe84c9076262e0f68ca3f6e6349e6f00cc5
den <kdv@groupbms.ru>
2023-06-21 11:33:40 +0300

    config server

--- speaker/src/main/java/ru/bmsgroup/speaker/SpeakerKafkaConfiguration.java
+++ speaker/src/main/java/ru/bmsgroup/speaker/SpeakerKafkaConfiguration.java
@@ -1,5 +1,6 @@
 package ru.bmsgroup.speaker;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
@@ -10,10 +11,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableKafkaStreams
 public class SpeakerKafkaConfiguration {
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
     @Bean
     public NewTopic speakerTube() {
         return TopicBuilder.name("SPEAKER_TUBE").build();
@@ -21,6 +30,14 @@ public class SpeakerKafkaConfiguration {
     @Bean
     public NewTopic speakerProcessTupe() {
         return TopicBuilder.name("SPEAKER_PROCESS").build();
+    }
+
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
     }
 
     @Bean
